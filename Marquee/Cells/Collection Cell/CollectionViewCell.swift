9e55d2f3f4a17197cb1a4d65a4fe444a1554abe5
JoshuaBayless <63886653+JoshuaBayless@users.noreply.github.com>
2022-05-10 19:23:32 -0700

    Adds shimmer animation to items that are in focus

--- Marquee/Cells/Collection Cell/CollectionViewCell.swift
+++ Marquee/Cells/Collection Cell/CollectionViewCell.swift
@@ -10,8 +10,8 @@ import UIKit
 class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var poster: UIImageView!
-    
-    
+    @IBOutlet weak var poster2: UIImageView!
+    @IBOutlet weak var animationView: UIView!
     
     
     static let identifier = "CollectionViewCell"
@@ -22,8 +22,6 @@ class CollectionViewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        
     }
 
     
@@ -31,11 +29,43 @@ class CollectionViewCell: UICollectionViewCell {
 
         coordinator.addCoordinatedAnimations({
             if self.isFocused {
+                self.gradientAnimation()
                 self.poster.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+                self.animationView.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+                
             }else{
                 self.poster.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                self.animationView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+                self.poster.layer.mask = nil
             }
         }, completion: nil)
+        
     
     }
+    
+    func gradientAnimation() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.clear.cgColor, UIColor.white.cgColor, UIColor.clear.cgColor, UIColor.clear.cgColor]
+        gradientLayer.locations = [0.0, 0.5, 1.0]
+        gradientLayer.frame = poster.frame
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 1.0)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradientLayer.opacity = 0.2
+        
+        poster.layer.addSublayer(gradientLayer)
+        
+        // animation
+        let animation = CABasicAnimation(keyPath: "transform.translation.x")
+        animation.duration = 0.3
+        animation.fromValue = -poster.frame.width
+        animation.toValue = poster.frame.width
+        gradientLayer.add(animation, forKey: "gradientAnimation")
+        //remove animation
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+            gradientLayer.removeFromSuperlayer()
+        })
+    }
 }
+
+
+
